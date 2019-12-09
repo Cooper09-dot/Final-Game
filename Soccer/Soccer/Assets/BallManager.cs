70d295bd6ad342515031edb7f08e445a1803882c
Jermaine09 <cooper906@morrisville.edu>
2019-12-09 11:12:07 -0500

    Final Game

---
+++ Soccer/Soccer/Assets/BallManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+    public int Score1;
+    public int Score2;
+    public void OnTriggerEnter(Collider other)
+    {
+        //If the ball goes into the goal
+        if (other.gameObject.tag == "Goal")
+        {
+            //teleport the ball back to it's starting point. 
+            transform.position = GameObject.Find("BallPosition").transform.position;
+            //Stop the ball from moving immediately after spawning.
+            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            //add to the scoreboard. 
+            Score1 += 1;
+        }
+        //of the ball goes into the other goal 
+        if (other.gameObject.tag == "Goal2")
+        {
+            transform.position = GameObject.Find("BallPosition").transform.position;
+            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            Score2 += 1;
+        }
+    }
+
+        public void Update()
+    {
+        //changes team 1's score
+            GameObject.Find("Score1").GetComponent<TextMesh>().text = Score1 + "";
+        //changes team 2's score
+            GameObject.Find("Score2").GetComponent<TextMesh>().text = Score2 + "";
+    }
+}
+
---
+++ Soccer/Soccer/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Gives the ability to load into the game. 
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    //Quit game
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
